6e984e91913ae9d5d1268c3f1eb3c851fcb41495
AliAkbar Naderian <naderianaliakbar@gmail.com>
2023-09-13 16:41:07 +0330

    fix some issue #C1

--- modules/db.js
+++ modules/db.js
@@ -3,7 +3,10 @@ let _db;
 
 module.exports = {
     connect: async function () {
-        const password = encodeURIComponent('f5lQmGwM1RjoAXAb');
+        const user     = encodeURIComponent(process.env.MongoDB_USER);
+        const password = encodeURIComponent(process.env.MongoDB_PASSWORD);
+        const host     = encodeURIComponent(process.env.MongoDB_HOST);
+        //const uri      = 'mongodb://' + user + ':' + password + '@' + host + ':27017/?authSource=admin';
         const uri      = `mongodb://0.0.0.0:27017`;
         const client   = new MongoClient(uri);
         await client.connect();
