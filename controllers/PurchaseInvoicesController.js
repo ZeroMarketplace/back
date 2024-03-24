0a76983daee206c1192d94c72565de3494f61493
AliAkbar Naderian <naderianaliakbar@gmail.com>
2024-03-24 17:42:43 +0330

    Feature: PurchaseInvoices UpdateOne

--- controllers/PurchaseInvoicesController.js
+++ controllers/PurchaseInvoicesController.js
@@ -10,48 +10,51 @@ class PurchaseInvoicesController extends Controllers {
         super();
     }
 
+    static async calculateInvoice($input) {
+        $input.total = 0;
+        $input.sum   = 0;
+
+        // calc products price
+        $input.products.forEach((product) => {
+            product.total = product.count * product.price.purchase;
+            $input.sum += product.total;
+        });
+
+        // calc add and subtract
+        let operationSum = 0;
+        $input.total     = $input.sum;
+        for (const addAndSub of $input.AddAndSub) {
+            // get add and subtract
+            let detailAddAndSubtract = await AddAndSubtractController.get(addAndSub.reason);
+            detailAddAndSubtract     = detailAddAndSubtract.data;
+
+            if (detailAddAndSubtract.operation === 'add') {
+                if (detailAddAndSubtract.type === 'percent') {
+                    operationSum = ($input.sum * addAndSub.value / 100)
+                    $input.total += operationSum;
+                } else {
+                    operationSum = Number(addAndSub.value);
+                    $input.total += addAndSub.value;
+                }
+            } else {
+                if (detailAddAndSubtract.type === 'percent') {
+                    operationSum = ($input.sum * addAndSub.value / 100)
+                    $input.total -= operationSum;
+                } else {
+                    operationSum = Number(addAndSub.value);
+                    $input.total -= addAndSub.value;
+                }
+            }
+
+            addAndSub.sum = operationSum;
+        }
+    }
+
     static insertOne($input) {
         return new Promise(async (resolve, reject) => {
             // check filter is valid ...
-
-            let code  = await CountersController.increment('purchase-invoices');
-            let total = 0;
-            let sum   = 0;
-
-            // calc products price
-            $input.products.forEach((product) => {
-                product.total = product.count * product.price.purchase;
-                sum += product.total;
-            });
-
-            // calc add and subtract
-            let operationSum = 0;
-            total            = sum;
-            for (const addAndSub of $input.AddAndSub) {
-                // get add and subtract
-                let detailAddAndSubtract = await AddAndSubtractController.get(addAndSub.reason);
-                detailAddAndSubtract     = detailAddAndSubtract.data;
-
-                if (detailAddAndSubtract.operation === 'add') {
-                    if (detailAddAndSubtract.type === 'percent') {
-                        operationSum = (sum * addAndSub.value / 100)
-                        total += operationSum;
-                    } else {
-                        operationSum = Number(addAndSub.value);
-                        total += addAndSub.value;
-                    }
-                } else {
-                    if (detailAddAndSubtract.type === 'percent') {
-                        operationSum = (sum * addAndSub.value / 100)
-                        total -= operationSum;
-                    } else {
-                        operationSum = Number(addAndSub.value);
-                        total -= addAndSub.value;
-                    }
-                }
-
-                addAndSub.sum = operationSum;
-            }
+            let code = await CountersController.increment('purchase-invoices');
+            await this.calculateInvoice($input);
 
             // filter
             this.model.insertOne({
@@ -62,7 +65,7 @@ class PurchaseInvoicesController extends Controllers {
                 description: $input.description,
                 products   : $input.products,
                 AddAndSub  : $input.AddAndSub,
-                total      : total,
+                total      : $input.total,
                 status     : 'active',
                 _user      : $input.user.data.id
             }).then(
@@ -71,6 +74,25 @@ class PurchaseInvoicesController extends Controllers {
                     return resolve({
                         code: 200,
                         data: response.toObject()
+                    });
+                },
+                (response) => {
+                    return reject(response);
+                });
+        });
+    }
+
+    static get($id) {
+        return new Promise((resolve, reject) => {
+            // check filter is valid and remove other parameters (just valid query by user role) ...
+
+            // filter
+            this.model.get($id).then(
+                (response) => {
+                    // check the result ... and return
+                    return resolve({
+                        code: 200,
+                        data: response
                     });
                 },
                 (response) => {
@@ -123,15 +145,19 @@ class PurchaseInvoicesController extends Controllers {
     }
 
     static updateOne($id, $input) {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             // check filter is valid ...
+            await this.calculateInvoice($input);
 
             // filter
             this.model.updateOne($id, {
-                title: {
-                    en: $input.title.en,
-                    fa: $input.title.fa
-                }
+                customer   : $input.customer,
+                dateTime   : $input.dateTime,
+                warehouse  : $input.warehouse,
+                description: $input.description,
+                products   : $input.products,
+                AddAndSub  : $input.AddAndSub,
+                total      : $input.total,
             }).then(
                 (response) => {
                     // check the result ... and return
