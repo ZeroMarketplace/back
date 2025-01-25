c9b4263a92c8a756d435e639fe6f393fbfb9f7fd
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-01-25 15:57:30 +0330

    Feature: improve InputsController for validate object and array

--- controllers/InputsController.js
+++ controllers/InputsController.js
@@ -25,88 +25,100 @@ class InputsController extends Controllers {
         return new Promise((resolve, reject) => {
             const errors = [];
 
-            for (const field in $schema) {
-                if ($schema.hasOwnProperty(field)) {
-                    const rules = $schema[field];
-                    const value = $input[field];
+            const validate = (input, schema, parentPath = '') => {
+                for (const field in schema) {
+                    if (schema.hasOwnProperty(field)) {
+                        const rules     = schema[field];
+                        const value     = input[field];
+                        const fieldPath = parentPath ? `${parentPath}.${field}` : field;
 
-                    // check required
-                    if (rules.required && (value === undefined || value === null || value === '')) {
-                        errors.push(`${field} is required`);
-                        continue;
-                    }
+                        // check required
+                        if (rules.required && (value === undefined || value === null || value === '')) {
+                            errors.push(`${fieldPath} is required`);
+                            continue;
+                        }
 
-                    // check if is not required but value is null
-                    if(!rules.required && (value === undefined)) {
-                        continue;
-                    }
+                        // check if is not required but value is null
+                        if (!rules.required && value === undefined) {
+                            continue;
+                        }
 
-                    // check type
-                    if (rules.type) {
-                        switch (rules.type) {
-                            case 'string':
-                                if (typeof value !== 'string') {
-                                    errors.push(`${field} must be a string`);
-                                    continue;
-                                }
-                                break;
-                            case 'mongoId':
-                                if (!validator.isMongoId(value)) {
-                                    errors.push(`${field} must be an Object Id`);
-                                }
-                                break;
-                            case 'number':
-                                if (!validator.isNumeric(value)) {
-                                    errors.push(`${field} must be a Number`);
-                                }
-                                break;
-                            case 'strongPassword':
-                                if (!validator.isStrongPassword(value)) {
-                                    errors.push(`${field} must be a Strong Password`);
-                                }
-                                break;
-                            case 'date':
-                                if (!validator.isDate(value)) {
-                                    errors.push(`${field} must be a Date`);
-                                }
-                                break;
-                            case 'email':
-                                // check is Email
-                                if (!validator.isEmail(value)) {
-                                    errors.push(`${field} must be a valid email`);
-                                }
-                                break;
-                            case 'phone':
-                                // check is Email
-                                const mobileRegex = /^(\+98|0)?9\d{9}$/;
-                                if(!mobileRegex.test(value)) {
-                                    errors.push(`${field} must be a valid email`);
-                                }
-                                break;
+                        // check type
+                        if (rules.type) {
+                            switch (rules.type) {
+                                case 'string':
+                                    if (typeof value !== 'string') {
+                                        errors.push(`${fieldPath} must be a string`);
+                                        continue;
+                                    }
+                                    break;
+                                case 'mongoId':
+                                    if (!validator.isMongoId(value)) {
+                                        errors.push(`${fieldPath} must be an Object Id`);
+                                    }
+                                    break;
+                                case 'number':
+                                    if (!validator.isNumeric(value)) {
+                                        errors.push(`${fieldPath} must be a Number`);
+                                    }
+                                    break;
+                                case 'strongPassword':
+                                    if (!validator.isStrongPassword(value)) {
+                                        errors.push(`${fieldPath} must be a Strong Password`);
+                                    }
+                                    break;
+                                case 'date':
+                                    if (!validator.isDate(value)) {
+                                        errors.push(`${fieldPath} must be a Date`);
+                                    }
+                                    break;
+                                case 'email':
+                                    if (!validator.isEmail(value)) {
+                                        errors.push(`${fieldPath} must be a valid email`);
+                                    }
+                                    break;
+                                case 'phone':
+                                    const mobileRegex = /^(\+98|0)?9\d{9}$/;
+                                    if (!mobileRegex.test(value)) {
+                                        errors.push(`${fieldPath} must be a valid phone number`);
+                                    }
+                                    break;
+                            }
+                        }
+
+                        // check allowed values
+                        if (rules.allowedValues && !rules.allowedValues.includes(value)) {
+                            errors.push(`${fieldPath} must be one of the following values: ${rules.allowedValues.join(', ')}`);
+                        }
+
+                        // check minLength
+                        if (rules.minLength && !validator.isLength(value, {min: rules.minLength})) {
+                            errors.push(`${fieldPath} must be at least ${rules.minLength} characters`);
+                        }
+
+                        // check maxLength
+                        if (rules.maxLength && !validator.isLength(value, {max: rules.maxLength})) {
+                            errors.push(`${fieldPath} must be at most ${rules.maxLength} characters`);
+                        }
+
+                        // check pattern
+                        if (rules.pattern && !new RegExp(rules.pattern).test(value)) {
+                            errors.push(`${fieldPath} does not match the pattern ${rules.pattern}`);
+                        }
+
+                        // handle nested objects or arrays
+                        if (rules.type === 'array' && Array.isArray(value) && rules.items) {
+                            value.forEach((item, index) => {
+                                validate(item, rules.items, `${fieldPath}[${index}]`);
+                            });
+                        } else if (rules.type === 'object' && typeof value === 'object' && rules.properties) {
+                            validate(value, rules.properties, fieldPath);
                         }
                     }
-
-                    // check allowed values
-                    if (rules.allowedValues && !rules.allowedValues.includes(value)) {
-                        errors.push(`${field} must be one of the following values: ${rules.allowedValues.join(', ')}`);
-                    }
-
-                    // check minLength
-                    if (rules.minLength && !validator.isLength(value, {min: rules.minLength})) {
-                        errors.push(`${field} must be at least ${rules.minLength} characters`);
-                    }
-
-                    // check maxLength
-                    if (rules.maxLength && !validator.isLength(value, {max: rules.maxLength})) {
-                        errors.push(`${field} must be at most ${rules.maxLength} characters`);
-                    }
-
-                    // check pattern
-                    if (rules.pattern && !new RegExp(rules.pattern).test(value)) {
-                        errors.push(`${field} does not match the pattern ${rules.pattern}`);
-                    }
                 }
-            }
+            };
+
+            validate($input, $schema);
 
             if (errors.length > 0) {
                 return reject({
@@ -119,8 +131,9 @@ class InputsController extends Controllers {
             } else {
                 return resolve($input);
             }
-        })
+        });
     }
+
 
     checkRequiredFields($input, $requiredFields = []) {
         return new Promise((resolve, reject) => {
