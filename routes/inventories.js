392cb3cc04546d38839b14b8ea63e6209d625f36
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-02-17 15:24:25 +0330

    Update: add Swagger documentation for inventories API and refactor list method call

--- routes/inventories.js
+++ routes/inventories.js
@@ -5,6 +5,89 @@ import InventoriesController from '../controllers/InventoriesController.js';
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * /api/inventories:
+ *   get:
+ *     summary: Get all Inventories
+ *     tags:
+ *       - Inventories
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: perPage
+ *         schema:
+ *           type: number
+ *       - in: query
+ *         name: sortColumn
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: sortDirection
+ *         schema:
+ *           type: number
+ *     responses:
+ *       400:
+ *          description: Bad Request (for validation)
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          message:
+ *                              type: string
+ *                          errors:
+ *                              type: array
+ *                              items:
+ *                                  type: string
+ *       200:
+ *         description: Successful get
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 total:
+ *                   type: number
+ *                 list:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       product:
+ *                         type: string
+ *                       productDetails:
+ *                         type: object
+ *                         properties:
+ *                           _id:
+ *                             type: string
+ *                           code:
+ *                             type: number
+ *                           barcode:
+ *                             type: string
+ *                           _unit:
+ *                             type: object
+ *                             properties:
+ *                               _id:
+ *                                 type: string
+ *                               title:
+ *                                 type: string
+ *                       warehouses:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             _id:
+ *                               type: string
+ *                             count:
+ *                               type: string
+ *                       total:
+ *                         type: number
+ *
+ */
 router.get(
     '/',
     AuthController.authorizeJWT,
@@ -13,7 +96,7 @@ router.get(
         // create clean input
         let $input = InputsController.clearInput(req.query);
 
-        InventoriesController.list($input).then(
+        InventoriesController.inventories($input).then(
             (response) => {
                 return res.status(response.code).json(response.data);
             },
@@ -24,24 +107,5 @@ router.get(
     }
 );
 
-router.get(
-    '/:_product',
-    AuthController.authorizeJWT,
-    AuthController.checkAccess,
-    function (req, res) {
-        // create clean input
-        let $params = InputsController.clearInput(req.params);
-        let $query  = InputsController.clearInput(req.query);
-
-        InventoriesController.getInventoryByProductId($params, $query).then(
-            (response) => {
-                return res.status(response.code).json(response.data);
-            },
-            (error) => {
-                return res.status(error.code ?? 500).json(error.data ?? {});
-            }
-        );
-    }
-);
 
 export default router;
