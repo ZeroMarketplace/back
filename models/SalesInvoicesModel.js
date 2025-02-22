5ec7dadae09246ec0dcf223dd1d37f81324416b5
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-02-22 15:10:47 +0330

    Update: enhance SalesInvoicesModel schema by adding required fields for validation and improving product structure

--- models/SalesInvoicesModel.js
+++ models/SalesInvoicesModel.js
@@ -5,31 +5,43 @@ class SalesInvoicesModel extends Models {
 
     // const Account = null;
     static schema = new Schema({
-            code       : Number,
-            _customer  : {type: Schema.Types.ObjectId, ref: 'users'},
-            dateTime   : Date,
+            code       : {type: Number, required: true},
+            _customer  : {type: Schema.Types.ObjectId, ref: 'users', required: true},
+            dateTime   : {type: Date, required: true},
             description: String,
-            products   : [
-                {
-                    _product         : {type: Schema.Types.ObjectId, ref: 'products'},
-                    count            : Number,
-                    price            : Number,
-                    total            : Number,
-                    _inventoryChanges: {type: Schema.Types.ObjectId, ref: 'inventory-changes'},
-                }
-            ],
+            products   : {
+                type    : [
+                    {
+                        _id              : {
+                            type    : Schema.Types.ObjectId,
+                            ref     : 'products',
+                            required: true
+                        },
+                        count            : {type: Number, required: true},
+                        price            : {type: Number, required: true},
+                        total            : {type: Number, required: true},
+                        _warehouse       : {type: Schema.Types.ObjectId, ref: 'warehouses', required: true},
+                        _inventoryChanges: {type: Schema.Types.ObjectId, ref: 'inventory-changes'},
+                    }
+                ],
+                required: true,
+            },
             AddAndSub  : [
                 {
-                    _reason: {type: Schema.Types.ObjectId, ref: 'add-and-subtract'},
-                    amount : Number,
-                    value  : Number,
+                    _reason: {
+                        type    : Schema.Types.ObjectId,
+                        ref     : 'add-and-subtract',
+                        required: true
+                    },
+                    amount : {type: Number, required: true},
+                    value  : {type: Number, required: true},
                 }
             ],
-            status     : {type: String, enum: ['Paid', 'Unpaid']},
-            total      : Number,
-            sum        : Number,
+            status     : {type: String, enum: ['Paid', 'Unpaid'], required: true},
+            total      : {type: Number, required: true},
+            sum        : {type: Number, required: true},
             _settlement: {type: Schema.Types.ObjectId, ref: 'settlements'},
-            _user      : {type: Schema.Types.ObjectId, ref: 'users'}
+            _user      : {type: Schema.Types.ObjectId, ref: 'users', required: true}
         },
         {timestamps: true});
 
