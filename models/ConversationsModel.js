96be3d283c43f7805361bb0e722555f9961c4f71
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-02-24 21:20:13 +0330

    Update: enhance error handling and logging in the application for better debugging

--- models/ConversationsModel.js
+++ models/ConversationsModel.js
@@ -5,9 +5,16 @@ class ConversationsModel extends Models {
 
     // const Account = null;
     static schema = new Schema({
-            type          : {type: String, enum: ['private', 'group', 'channel', 'personal', 'support']},
+            type          : {
+                type    : String,
+                enum    : ['private', 'group', 'channel', 'personal', 'support'],
+                required: true,
+            },
             name          : String, // groups and channels
-            members       : [{type: Schema.Types.ObjectId, ref: 'users'}],
+            members       : {
+                type    : [{type: Schema.Types.ObjectId, ref: 'users'}],
+                required: true
+            },
             admins        : { // groups and channels
                 type   : [{type: Schema.Types.ObjectId, ref: 'users'}],
                 default: undefined
@@ -25,164 +32,168 @@ class ConversationsModel extends Models {
         super('conversations', ConversationsModel.schema);
     }
 
-    listOfConversations($filter, $options, $userId) {
-        return new Promise((resolve, reject) => {
-            const aggregationQuery = [
-                // مرحله 1: فیلتر کردن گفتگوهای کاربر
-                {
-                    $match: {
-                        members: $userId,
-                        _deletedFor: {
-                            $nin: [$userId]
+    conversations($filter, $options) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // init the query
+                const aggregationQuery = [
+                    // filter user conversations
+                    {
+                        $match: {
+                            members    : $filter._user,
+                            _deletedFor: {
+                                $nin: [$filter._user]
+                            }
                         }
-                    }
-                },
-                // مرحله 2: دریافت اطلاعات آخرین پیام
-                {
-                    $lookup: {
-                        from        : 'messages', // نام کالکشن پیام‌ها
-                        localField  : '_id',
-                        foreignField: '_conversation',
-                        as          : 'lastMessage'
-                    }
-                },
-                {
-                    $unwind: {
-                        path                      : '$lastMessage',
-                        preserveNullAndEmptyArrays: true
-                    }
-                },
-                // remove deleted Messages
-                {
-                    $match: {
-                        'lastMessage._deletedFor': {
-                            $nin: [$userId]
+                    },
+                    // get the last message of every conversation
+                    {
+                        $lookup: {
+                            from        : 'messages',
+                            localField  : '_id',
+                            foreignField: '_conversation',
+                            as          : 'lastMessage'
                         }
-                    }
-                },
-                // مرتب‌سازی پیام‌ها بر اساس زمان ارسال و فقط آخرین پیام را نگه می‌داریم
-                {
-                    $sort: {
-                        'lastMessage.createdAt': -1
-                    }
-                },
-                {
-                    $group: {
-                        _id             : '$_id',
-                        conversationData: {$first: '$$ROOT'}, // اطلاعات گفتگو
-                        lastMessage     : {$first: '$lastMessage'} // آخرین پیام
-                    }
-                },
-                {
-                    $addFields: {
-                        'conversationData.lastMessage': '$lastMessage'
-                    }
-                },
-                {
-                    $replaceRoot: {
-                        newRoot: '$conversationData'
-                    }
-                },
-                // مرحله 3: دریافت اطلاعات اعضا فقط اگر نوع گفتگو یکی از موارد مورد نظر باشد
-                {
-                    $lookup: {
-                        from    : 'users',
-                        let     : {memberIds: '$members', conversationType: '$type'},
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $and: [
-                                            {$in: ['$$conversationType', ['private', 'group', 'personal', 'support']]}, // شرط برای نوع گفتگو
-                                            {$in: ['$_id', '$$memberIds']},
-                                            {$ne: ['$_id', $userId]} // حذف خود کاربر
-                                        ]
+                    },
+                    {
+                        $unwind: {
+                            path                      : '$lastMessage',
+                            preserveNullAndEmptyArrays: true
+                        }
+                    },
+                    // remove deleted Messages
+                    {
+                        $match: {
+                            'lastMessage._deletedFor': {
+                                $nin: [$filter._user]
+                            }
+                        }
+                    },
+                    // sort all messasges by createdAt
+                    {
+                        $sort: {
+                            'lastMessage.createdAt': -1
+                        }
+                    },
+                    {
+                        $group: {
+                            _id             : '$_id',
+                            conversationData: {$first: '$$ROOT'}, // conversation data
+                            lastMessage     : {$first: '$lastMessage'} // last message data
+                        }
+                    },
+                    {
+                        $addFields: {
+                            'conversationData.lastMessage': '$lastMessage'
+                        }
+                    },
+                    {
+                        $replaceRoot: {
+                            newRoot: '$conversationData'
+                        }
+                    },
+                    // get the members' details if type validation is true
+                    {
+                        $lookup: {
+                            from    : 'users',
+                            let     : {memberIds: '$members', conversationType: '$type'},
+                            pipeline: [
+                                {
+                                    $match: {
+                                        $expr: {
+                                            $and: [
+                                                {$in: ['$$conversationType', ['private', 'group', 'personal', 'support']]}, // validate for types
+                                                {$in: ['$_id', '$$memberIds']},
+                                                {$ne: ['$_id', $filter._user]} // remove user self data
+                                            ]
+                                        }
+                                    }
+                                },
+                                {
+                                    $project: {
+                                        _id      : 1,
+                                        firstName: 1,
+                                        lastName : 1,
+                                        avatars  : 1,
+                                        color    : 1
                                     }
                                 }
-                            },
-                            {
-                                $project: {
-                                    _id    : 1,
-                                    name   : 1,
-                                    avatars: 1,
-                                    color  : 1
-                                }
-                            }
-                        ],
-                        as      : 'memberDetails'
-                    }
-                },
-                // مرحله 3: محاسبه تعداد پیام‌های ناخوانده
-                {
-                    $lookup: {
-                        from    : 'messages',
-                        let     : {conversationId: '$_id'},
-                        pipeline: [
-                            {
-                                $match: {
-                                    $expr: {
-                                        $and: [
-                                            {$eq: ['$_conversation', '$$conversationId']},
-                                            {$ne: ['$sender', $userId]},
-                                            {$not: {$in: [$userId, '$_readBy']}}
-                                        ]
+                            ],
+                            as      : 'members'
+                        }
+                    },
+                    // calc unread messages count
+                    {
+                        $lookup: {
+                            from    : 'messages',
+                            let     : {conversationId: '$_id'},
+                            pipeline: [
+                                {
+                                    $match: {
+                                        $expr: {
+                                            $and: [
+                                                {$eq: ['$_conversation', '$$conversationId']},
+                                                {$ne: ['$sender', $filter]},
+                                                {$not: {$in: [$filter, '$_readBy']}}
+                                            ]
+                                        }
                                     }
+                                },
+                                {
+                                    $count: 'unreadCount'
                                 }
-                            },
-                            {
-                                $count: 'unreadCount'
-                            }
-                        ],
-                        as      : 'unreadCount'
-                    }
-                },
-                {
-                    $addFields: {
-                        unreadCount: {
-                            $cond: {
-                                if  : {$gt: [{$size: '$unreadCount'}, 0]},
-                                then: {$arrayElemAt: ['$unreadCount.unreadCount', 0]},
-                                else: 0
+                            ],
+                            as      : 'unreadCount'
+                        }
+                    },
+                    {
+                        $addFields: {
+                            unreadCount: {
+                                $cond: {
+                                    if  : {$gt: [{$size: '$unreadCount'}, 0]},
+                                    then: {$arrayElemAt: ['$unreadCount.unreadCount', 0]},
+                                    else: 0
+                                }
                             }
                         }
+                    },
+                    {
+                        $project: {
+                            lastMessage   : 1,
+                            unreadCount   : 1,
+                            _id           : 1,
+                            type          : 1,
+                            name          : 1,
+                            members       : 1,
+                            admins        : 1,
+                            _owner        : 1,
+                            description   : 1,
+                            avatars       : 1,
+                            _pinnedMessage: 1,
+                            settings      : 1,
+                            updatedAt     : 1,
+                            createdAt     : 1,
+                        }
                     }
-                },
-                {
-                    $project: {
-                        lastMessage   : 1,
-                        unreadCount   : 1,
-                        _id           : 1,
-                        type          : 1,
-                        name          : 1,
-                        members       : 1,
-                        admins        : 1,
-                        _owner        : 1,
-                        description   : 1,
-                        avatars       : 1,
-                        _pinnedMessage: 1,
-                        settings      : 1,
-                        updatedAt     : 1,
-                        memberDetails : 1
-                    }
+                ];
+
+                // exec the query
+                let response = await this.collectionModel.aggregate(aggregationQuery);
+
+                // return result
+                if (response) {
+                    return resolve(response);
+                } else {
+                    return resolve([]);
                 }
-            ];
-
-            this.collectionModel.aggregate(aggregationQuery).then(
-                (response) => {
-                    if (response) {
-                        return resolve(response);
-                    } else {
-                        return resolve([]);
+            } catch (error) {
+                return reject({
+                    code: 500,
+                    data: {
+                        errors: [error]
                     }
-                },
-                (error) => {
-                    console.log(error);
-                    return reject({
-                        code: 500
-                    });
-                },
-            );
-
+                });
+            }
         });
     }
 
