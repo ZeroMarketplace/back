c287c5f37bfe3b87fadad84939333acc7e1098e2
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-03-06 18:13:45 +0330

    Update: enhance error handling in Inputs Controller for improved user feedback

--- controllers/InputsController.js
+++ controllers/InputsController.js
@@ -13,13 +13,14 @@ class InputsController extends Controllers {
         if ($input) {
             // clear every key and index
             for (const [$index, $value] of Object.entries($input)) {
-                if (typeof $value === 'object') {
-                    // clean every child
-                    $input[$index] = this.clearInput($value);
-                } else {
-                    // check xss
-                    if (typeof $input[$index] === 'string')
+                switch (typeof $value) {
+                    case 'object':
+                        // clean every child
+                        $input[$index] = this.clearInput($value);
+                        break;
+                    case "string":
                         $input[$index] = validator.escape($value);
+                        break;
                 }
             }
         }
@@ -27,165 +28,167 @@ class InputsController extends Controllers {
         return $input;
     }
 
-    static validateInput($input, $schema) {
-        return new Promise((resolve, reject) => {
-            const errors = [];
+    static validateInput($input, $schema, $options = {strict: false}) {
+        const errors = [];
 
-            const validate = (input, schema, parentPath = '') => {
-                for (const field in schema) {
-                    if (schema.hasOwnProperty(field)) {
-                        const rules     = schema[field];
-                        let value       = input[field];
-                        const fieldPath = parentPath ? `${parentPath}.${field}` : field;
+        const validate = ($inputObject, $schemaOfObject, $parentPath = '', $recursive = false) => {
+            // Iterate through each field in the schema
+            for (const field in $schemaOfObject) {
+                if ($schemaOfObject.hasOwnProperty(field)) {
+                    const rules   = $schemaOfObject[field];
+                    let value     = $inputObject[field];
+                    let fieldPath = $parentPath ? `${$parentPath}.${field}` : field;
 
-                        // check required
-                        if (rules.required && (value === undefined || value === null || value === '')) {
-                            errors.push(`${fieldPath} is required`);
-                            continue;
+                    // fix the exception for array item validation
+                    if ($recursive) {
+                        fieldPath = $parentPath ? `${$parentPath}` : field;
+                    }
+
+                    // Check for required field
+                    if (rules.required && (value === undefined || value === null || value === '')) {
+                        errors.push(`${fieldPath} is required`);
+                        continue;
+                    }
+
+                    // Skip undefined if field is not required
+                    if (!rules.required && value === undefined) {
+                        continue;
+                    }
+
+                    // Type validation
+                    if (rules.type) {
+                        switch (rules.type) {
+                            case 'string':
+                                if (typeof value !== 'string') {
+                                    errors.push(`${fieldPath} must be a string`);
+                                    continue;
+                                }
+                                break;
+
+                            case 'mongoId':
+                                if (!mongoose.isValidObjectId(value)) {
+                                    errors.push(`${fieldPath} must be a valid ObjectId`);
+                                }
+                                break;
+
+                            case 'number':
+                                if (isNaN(value)) {
+                                    errors.push(`${fieldPath} must be a number`);
+                                }
+                                break;
+
+                            case 'strongPassword':
+                                if (!validator.isStrongPassword(value)) {
+                                    errors.push(`${fieldPath} must be a strong password`);
+                                }
+                                break;
+
+                            case 'date':
+                                if (!validator.isDate(String(value))) { // Convert to string before validation
+                                    errors.push(`${fieldPath} must be a valid date`);
+                                }
+                                break;
+
+                            case 'email':
+                                if (!validator.isEmail(value)) {
+                                    errors.push(`${fieldPath} must be a valid email`);
+                                }
+                                break;
+
+                            case 'phone':
+                                const mobileRegex = /^(\+98|0)?9\d{9}$/;
+                                if (!mobileRegex.test(value)) {
+                                    errors.push(`${fieldPath} must be a valid phone number`);
+                                }
+                                break;
+
+                            case 'array':
+                                // Check if the value is an actual array
+                                if (!Array.isArray(value)) {
+                                    errors.push(`${fieldPath} must be an array`);
+                                    break;
+                                }
+
+                                // Validate min item count in array
+                                if (rules.minItemCount && value.length < rules.minItemCount) {
+                                    errors.push(`${fieldPath} must have at least ${rules.minItemCount} items`);
+                                }
+
+                                // Validate max item count in array
+                                if (rules.maxItemCount && value.length > rules.maxItemCount) {
+                                    errors.push(`${fieldPath} must have at most ${rules.maxItemCount} items`);
+                                }
+
+                                // Validate each item in the array using the defined "items" schema
+                                if (rules.items) {
+                                    value.forEach((item, index) => {
+                                        validate(
+                                            {[field]: item},
+                                            {[field]: rules.items},
+                                            `${fieldPath}[${index}]`,
+                                            true
+                                        );
+                                    });
+                                }
+                                break;
+
+                            case 'object':
+                                // Ensure value is an object and not null or an array
+                                if (typeof value !== 'object' || value === null || Array.isArray(value)) {
+                                    errors.push(`${fieldPath} must be an object`);
+                                    break;
+                                }
+
+                                // Validate properties of the object if specified
+                                if (rules.properties) {
+                                    validate(value, rules.properties, fieldPath);
+                                }
+
+                                // Check for extra properties in the object if 'strict' mode is enabled
+                                if ($options.strict && Object.keys(value).length !== Object.keys(rules.properties).length) {
+                                    errors.push(`${fieldPath} contains extra properties`);
+                                }
+                                break;
                         }
+                    }
 
-                        // check if is not required but value is null
-                        if (!rules.required && value === undefined) {
-                            continue;
-                        }
+                    // Check if value is among allowed values
+                    if (rules.allowedValues && !rules.allowedValues.includes(value)) {
+                        errors.push(`${fieldPath} must be one of: ${rules.allowedValues.join(', ')}`);
+                    }
 
-                        // check type
-                        if (rules.type) {
-                            switch (rules.type) {
-                                case 'string':
-                                    if (typeof value !== 'string') {
-                                        errors.push(`${fieldPath} must be a string`);
-                                        continue;
-                                    }
-                                    break;
-                                case 'mongoId':
-                                    if (!mongoose.isValidObjectId(value)) {
-                                        errors.push(`${fieldPath} must be an Object Id`);
-                                    }
-                                    break;
-                                case 'number':
-                                    value = Number(value);
-                                    if (typeof value !== 'number' || isNaN(value)) {
-                                        errors.push(`${fieldPath} must be a Number`);
-                                    } else {
-                                        // set the number
-                                        $input[field] = value;
-                                    }
-                                    break;
-                                case 'strongPassword':
-                                    if (!validator.isStrongPassword(value)) {
-                                        errors.push(`${fieldPath} must be a Strong Password`);
-                                    }
-                                    break;
-                                case 'date':
-                                    let date = new Date(value);
-                                    if (!validator.isDate(date)) {
-                                        errors.push(`${fieldPath} must be a Date`);
-                                    }
-                                    break;
-                                case 'email':
-                                    if (!validator.isEmail(value)) {
-                                        errors.push(`${fieldPath} must be a valid email`);
-                                    }
-                                    break;
-                                case 'phone':
-                                    const mobileRegex = /^(\+98|0)?9\d{9}$/;
-                                    if (!mobileRegex.test(value)) {
-                                        errors.push(`${fieldPath} must be a valid phone number`);
-                                    }
-                                    break;
-                                case 'array':
-                                    // Convert string to array if necessary (e.g., for query parameters)
-                                    if (typeof value === 'string') {
-                                        value = value.split(',');
-                                        $input[field] = value;
-                                    }
+                    // Validate minLength
+                    if (rules.minLength && !validator.isLength(value, {min: rules.minLength})) {
+                        errors.push(`${fieldPath} must be at least ${rules.minLength} characters`);
+                    }
 
-                                    if (!Array.isArray(value)) {
-                                        errors.push(`${fieldPath} must be a valid array`);
-                                        break;
-                                    }
+                    // Validate maxLength
+                    if (rules.maxLength && !validator.isLength(value, {max: rules.maxLength})) {
+                        errors.push(`${fieldPath} must be at most ${rules.maxLength} characters`);
+                    }
 
-                                    // Check minimum item count
-                                    if (rules.minItemCount && value.length < rules.minItemCount) {
-                                        errors.push(`${fieldPath} must have at least ${rules.minItemCount} items`);
-                                    }
-
-                                    // Check maximum item count
-                                    if (rules.maxItemCount && value.length > rules.maxItemCount) {
-                                        errors.push(`${fieldPath} must have at most ${rules.maxItemCount} items`);
-                                    }
-
-                                    if (rules.allowedValues) {
-                                        value.forEach((item, index) => {
-                                            const itemPath = `${fieldPath}[${index}]`;
-
-                                            // Validate each item based on allowedValues
-                                            if (rules.allowedValues && !rules.allowedValues.includes(item)) {
-                                                errors.push(`${itemPath} must be one of ${rules.allowedValues.join(', ')}`);
-                                            }
-                                        });
-                                    }
-
-                                    // Validate each item in the array
-                                    if (rules.items) {
-                                        value.forEach((item, index) => {
-                                            validate(item || {}, rules.items, `${fieldPath}[${index}]`);
-                                        });
-                                    }
-                                    break;
-
-                                case 'object':
-                                    // Ensure value is an object and not null
-                                    if (typeof value !== 'object' || value === null || Array.isArray(value)) {
-                                        errors.push(`${fieldPath} must be a valid object`);
-                                        break;
-                                    }
-
-                                    if (rules.properties) {
-                                        validate(value, rules.properties, fieldPath);
-                                    }
-                                    break;
-                            }
-                        }
-
-                        // check allowed values
-                        if (rules.allowedValues && !rules.allowedValues.includes(value)) {
-                            errors.push(`${fieldPath} must be one of the following values: ${rules.allowedValues.join(', ')}`);
-                        }
-
-                        // check minLength
-                        if (rules.minLength && !validator.isLength(value, {min: rules.minLength})) {
-                            errors.push(`${fieldPath} must be at least ${rules.minLength} characters`);
-                        }
-
-                        // check maxLength
-                        if (rules.maxLength && !validator.isLength(value, {max: rules.maxLength})) {
-                            errors.push(`${fieldPath} must be at most ${rules.maxLength} characters`);
-                        }
-
-                        // check pattern
-                        if (rules.pattern && !new RegExp(rules.pattern).test(value)) {
-                            errors.push(`${fieldPath} does not match the pattern ${rules.pattern}`);
-                        }
+                    // Check if value matches the specified pattern
+                    if (rules.pattern && !new RegExp(rules.pattern).test(value)) {
+                        errors.push(`${fieldPath} does not match the pattern ${rules.pattern}`);
                     }
                 }
-            };
-
-            validate($input, $schema);
-
-            if (errors.length > 0) {
-                return reject({
-                    code: 400,
-                    data: {
-                        message: 'Validation error',
-                        errors : errors
-                    }
-                });
-            } else {
-                return resolve($input);
             }
-        });
+        };
+
+        validate($input, $schema); // Start the validation
+
+        // Return the result of validation or throw errors if found
+        if (errors.length > 0) {
+            throw {
+                code: 400,
+                data: {
+                    message: 'Validation error',
+                    errors : errors
+                }
+            };
+        } else {
+            return $input; // Return the validated input if no errors
+        }
     }
 
 }
