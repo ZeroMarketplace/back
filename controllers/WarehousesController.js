5c49389d1b21957ad73fe9377d3f1a85f626ec78
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-03-08 18:42:34 +0330

    Update: enhance WarehousesController to set default status for search, implement setStatus method, and improve validation for typeOfSales

--- controllers/WarehousesController.js
+++ controllers/WarehousesController.js
@@ -2,6 +2,7 @@ import Controllers      from "../core/Controllers.js";
 import WarehousesModel  from "../models/WarehousesModel.js";
 import InputsController from "./InputsController.js";
 import persianDate      from "persian-date";
+import UnitsModel       from "../models/UnitsModel.js";
 
 class WarehousesController extends Controllers {
     static model = new WarehousesModel();
@@ -16,10 +17,35 @@ class WarehousesController extends Controllers {
         // pagination
         this.detectPaginationAndSort($input);
 
+        // set the default status for search
+        $query['status'] = WarehousesModel.statuses.ACTIVE;
+
         for (const [$index, $value] of Object.entries($input)) {
             switch ($index) {
                 case "title":
                     $query[$index] = {$regex: ".*" + $value + ".*"};
+                    break;
+                case 'statuses':
+                    // check if its admin
+                    if ($input.user.data.role === 'admin') {
+                        // convert statuses to array
+                        let $arrayOfValue = $value.split(',');
+                        let $statuses     = [];
+
+                        // add each status
+                        $arrayOfValue.forEach(status => {
+                            // if status is a valid number
+                            if (!isNaN(status)) {
+                                // add to array
+                                $statuses.push(Number(status));
+                            }
+                        })
+
+                        // set the filed for query
+                        if ($statuses.length > 1) {
+                            $query['status'] = {$in: $statuses};
+                        }
+                    }
                     break;
             }
         }
@@ -42,7 +68,7 @@ class WarehousesController extends Controllers {
                     title      : $input.title,
                     onlineSales: $input.onlineSales,
                     retail     : $input.retail,
-                    status     : "active",
+                    status     : WarehousesModel.statuses.ACTIVE,
                     _user      : $input.user.data._id,
                 });
 
@@ -65,6 +91,7 @@ class WarehousesController extends Controllers {
                 // validate Input
                 await InputsController.validateInput($input, {
                     title        : {type: "string"},
+                    statuses     : {type: "string"},
                     perPage      : {type: "number"},
                     page         : {type: "number"},
                     sortColumn   : {type: "string"},
@@ -108,6 +135,34 @@ class WarehousesController extends Controllers {
         });
     }
 
+    static getDefaultFor($input) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                await InputsController.validateInput($input, {
+                    typeOfSales: {
+                        type         : 'number',
+                        allowedValues: Object.values(WarehousesModel.defaultTypes),
+                        required     : true
+                    }
+                });
+
+                // find default for this $typeOfSales
+                let response = await this.model.item({
+                    defaultFor: Number($input.typeOfSales)
+                });
+
+                response = await this.outputBuilder(response.toObject());
+
+                return resolve({
+                    code: 200,
+                    data: response
+                });
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+
     static updateOne($input) {
         return new Promise(async (resolve, reject) => {
             try {
@@ -138,7 +193,34 @@ class WarehousesController extends Controllers {
         });
     }
 
-    // set default of warehouse (type)
+    static setStatus($input) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                // validate $input
+                InputsController.validateInput($input, {
+                    _id   : {type: 'mongoId', required: true},
+                    status: {
+                        type         : 'number',
+                        allowedValues: Object.values(WarehousesModel.statuses),
+                        required     : true
+                    },
+                });
+
+                // set the status
+                await this.model.updateOne($input._id, {
+                    status: $input.status
+                });
+
+                // return result
+                return resolve({
+                    code: 200
+                })
+            } catch (error) {
+                return reject(error);
+            }
+        })
+    }
+
     static setDefaultFor($input) {
         return new Promise(async (resolve, reject) => {
             try {
@@ -146,8 +228,8 @@ class WarehousesController extends Controllers {
                 await InputsController.validateInput($input, {
                     _id        : {type: 'mongoId', required: true},
                     typeOfSales: {
-                        type         : 'string',
-                        allowedValues: ['retail', 'onlineSales'],
+                        type         : 'number',
+                        allowedValues: Object.values(WarehousesModel.defaultTypes),
                         required     : true
                     }
                 });
@@ -155,71 +237,40 @@ class WarehousesController extends Controllers {
                 // find last default for this $typeOfSales
                 await this.model.item({defaultFor: $input.typeOfSales}).then(
                     async (responseFind) => {
-                        // update old default to null
+                        // remove the old default warehouse
                         responseFind.defaultFor = undefined;
                         await responseFind.save();
 
-                        // update new default
-                        let update                 = {
-                            defaultFor: $input.typeOfSales,
-                        };
-                        update[$input.typeOfSales] = true;
-                        await this.model.updateOne($input._id, update);
-
-                        return resolve({
-                            code: 200,
-                        });
                     },
-                    async (response) => {
-                        // update default
-                        let update                 = {
-                            defaultFor: $input.typeOfSales,
-                        };
-                        update[$input.typeOfSales] = true;
-                        await this.model.updateOne($input._id, update);
-
-                        return resolve({
-                            code: 200,
-                        });
+                    (notFound) => {
+                        // do nothing
                     }
                 );
 
-            } catch (error) {
-                return reject(error);
-            }
-        });
-    }
-
-    // get default of warehouse (type)
-    static getDefaultFor($input) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await InputsController.validateInput($input, {
-                    typeOfSales: {
-                        type         : 'string',
-                        allowedValues: ['retail', 'onlineSales'],
-                        required     : true
-                    }
-                });
-
-                // find default for this $typeOfSales
-                let response = await this.model.item({
+                // set the new default warehouse for $typeOfSales
+                // init the update variables
+                let update = {
                     defaultFor: $input.typeOfSales
-                });
+                };
 
-                // handle not found
-                if (!response) {
-                    return reject({
-                        code: 404
-                    });
+                // enable the type of sale flag for warehouse
+                switch ($input.typeOfSales) {
+                    case 1:
+                        update['retail'] = true;
+                        break;
+                    case 2:
+                        update['onlineSales'] = true;
+                        break;
                 }
 
-                response = await this.outputBuilder(response.toObject());
+                // update warehouse in db
+                await this.model.updateOne($input._id, update);
 
+                // return result
                 return resolve({
                     code: 200,
-                    data: response
                 });
+
             } catch (error) {
                 return reject(error);
             }
