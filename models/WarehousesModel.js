2b7aaf123856bd0d863cec8ae819854dd35dc8d8
AliAkbar Naderian <naderianaliakbar@gmail.com>
2025-03-08 18:42:19 +0330

    Update: modify WarehousesModel schema to change status and defaultFor types from string to number and define status constants

--- models/WarehousesModel.js
+++ models/WarehousesModel.js
@@ -2,15 +2,36 @@ import Models   from '../core/Models.js';
 import {Schema} from 'mongoose';
 
 class WarehousesModel extends Models {
-    // const Account = null;
+
+    static statuses = {
+        ACTIVE  : 1,
+        INACTIVE: 2,
+    };
+
+    static defaultTypes = {
+        RETAIL      : 1,
+        ONLINE_SALES: 2
+    };
+
     static schema = new Schema(
         {
             title      : {type: String, required: true},
             onlineSales: {type: Boolean, required: true},
             retail     : {type: Boolean, required: true},
-            defaultFor : {type: String, enum: ["retail", "onlineSales"]},
-            status     : {type: String, enum: ["active", "inactive"], required: true},
-            _user      : {type: Schema.Types.ObjectId, ref: "users", required: true},
+            defaultFor : {
+                type: Number,
+                enum: Object.values(WarehousesModel.defaultTypes)
+            },
+            status     : {
+                type    : Number,
+                enum    : Object.values(WarehousesModel.statuses),
+                required: true
+            },
+            _user      : {
+                type    : Schema.Types.ObjectId,
+                ref     : "users",
+                required: true
+            },
         },
         {timestamps: true}
     );
